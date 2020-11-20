4384b05d037fd871212a9f9c24377805872e6c43
Guangwei Zhou <40056835+GZHOUW@users.noreply.github.com>
2020-11-20 14:17:23 -0500

    Update system.cpp

--- src/system.cpp
+++ src/system.cpp
@@ -17,10 +17,12 @@ using std::size_t;
 using std::string;
 using std::vector;
 
+// Return the system's CPU
 Processor& System::Cpu() { 
     static Processor cpu;
     return cpu;}
 
+// Return a container composed of the system's processes
 vector<Process>& System::Processes() {
     processes.clear();
     for (int pid : LinuxParser::Pids()) {
@@ -31,31 +33,37 @@ vector<Process>& System::Processes() {
     return processes;
 }
 
+// Return the system's kernel identifier (string)
 std::string System::Kernel() {
   kernel = LinuxParser::Kernel();
   return kernel;
 }
 
+// Return the system's memory utilization
 float System::MemoryUtilization() {
   mem_utilization = LinuxParser::MemoryUtilization();
   return mem_utilization;
 }
 
+// Return the operating system name
 std::string System::OperatingSystem() {
   operating_system_ = LinuxParser::OperatingSystem();
   return operating_system_;
 }
 
+// Return the number of processes actively running on the system
 int System::RunningProcesses() {
   procs_running = LinuxParser::RunningProcesses();
   return procs_running;
 }
 
+// Return the total number of processes on the system
 int System::TotalProcesses() {
   total_processes = LinuxParser::TotalProcesses();
   return total_processes;
 }
 
+// Return the number of seconds since the system started running
 long System::UpTime() {
     uptime = LinuxParser::UpTime();
     return uptime;
